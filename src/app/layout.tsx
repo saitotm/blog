3473b1d15b72dfd5ed6126cee716a2785f94da88
saitotm <tomokazu.ml.tech@gmail.com>
2024-07-03 00:00:07 +0900

    Update to display github and X icons on the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import XIcon from '@mui/icons-material/X';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,6 +25,14 @@ export default function RootLayout({
           <Link href="/" className="text-3xl font-bold text-gray-800 pl-[50px]">
             {metadata.title as string}
            </Link>
+          <div className="grow flex justify-end mr-10">
+            <Link href="https://twitter.com/saitotm4423" className="px-2">
+              <XIcon />
+            </Link>
+            <Link href="https://github.com/saitotm" className="px-2">
+              <GitHubIcon />
+            </Link>
+          </div>
         </header>
         {children}
       </body>
